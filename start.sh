2a8a9ba3e76480ea791bd0f05026b489acacd088
p4yl0ad <57832389+p4yl0ad@users.noreply.github.com>
2023-01-13 19:24:34 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,5 +8,6 @@ sudo docker run -d -v /in:/in -v /out:/out -it adexplorersnapshotdockerwatchman
 
 
 # misc useful
+# sudo docker stop $(sudo docker ps -a -q)
 # sudo docker rm $(sudo docker ps -a -q)
 # sudo docker rmi $(sudo docker images -a -q) 
