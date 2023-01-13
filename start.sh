8a57baf5d25179c8675450f76e1df73954a6fe49
p4yl0ad <57832389+p4yl0ad@users.noreply.github.com>
2023-01-13 19:23:52 +0000

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+sudo echo "[i] Starting docker build && docker run "
+sudo docker build -t adexplorersnapshotdockerwatchman .
+sudo docker run -d -v /in:/in -v /out:/out -it adexplorersnapshotdockerwatchman
+
+
+
+
+
+
+# misc useful
+# sudo docker rm $(sudo docker ps -a -q)
+# sudo docker rmi $(sudo docker images -a -q) 
